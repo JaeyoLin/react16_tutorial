584712c29a5bb215f063a11f3f1260f9842ae5eb
Aaron Lin <aaron_lin@wistron.com>
2018-05-28 16:13:09 +0800

    Modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
   module: {
     rules: [
       {
@@ -11,27 +19,22 @@ module.exports = {
           loader: "babel-loader"
         }
       }, {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      }, {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
     new HtmlWebPackPlugin({
+      inject: false,
+      hash: true,
       template: "./src/index.html",
       filename: "./index.html"
     }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+    new WebpackMd5Hash()
   ]
 };
